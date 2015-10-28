0b8234bad4fcd8039bb9f9150a346fed7b980a39
cjiong <416137318@qq.com>
2015-10-28 21:31:52 +0800

    Initial Commit

---
+++ VideoLoginPageTests/VideoLoginPageTests.swift
@@ -0,0 +1,36 @@
+//
+//  VideoLoginPageTests.swift
+//  VideoLoginPageTests
+//
+//  Created by 陈炯 on 15/10/28.
+//  Copyright (c) 2015年 Jiong. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class VideoLoginPageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
